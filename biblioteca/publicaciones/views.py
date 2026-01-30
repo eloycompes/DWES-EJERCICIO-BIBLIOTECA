4cee2f09cac5cb18502a68e9195b801d1a3c5936
Eloy Compés <eloycompes@gmail.com>
2026-01-30 13:42:46 +0100

    eloy: creada app 'prestamos' con su modelo y vista

--- biblioteca/publicaciones/views.py
+++ biblioteca/publicaciones/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from biblioteca.publicaciones.models import Autor
+from .models import Autor
 
 # Create your views here.
 class autores_lista(ListView):
